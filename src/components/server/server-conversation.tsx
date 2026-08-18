90af1a61737e871b6e86da2e3d4cde63037c90f0
TheStami <a15@int.pl>
2026-08-18 10:27:51 +0200

    Close private message by scroll click

--- src/components/server/server-conversation.tsx
+++ src/components/server/server-conversation.tsx
@@ -38,9 +38,24 @@ export const ServerConversation = ({
     }
   };
 
+  const onMouseDown = (e: React.MouseEvent) => {
+    if (e.button === 1) {
+      e.preventDefault();
+    }
+  };
+
+  const onAuxClick = (e: React.MouseEvent) => {
+    if (e.button === 1) {
+      e.preventDefault();
+      onClose(e);
+    }
+  };
+
   return (
     <button
       onClick={onClick}
+      onMouseDown={onMouseDown}
+      onAuxClick={onAuxClick}
       className={cn(
         "group px-2 py-2 rounded-md flex items-center gap-x-2 w-full hover:bg-zinc-700/10 dark:hover:bg-zinc-700/50 transition mb-1",
         isSelected && "bg-zinc-700/20 dark:bg-zinc-700"
