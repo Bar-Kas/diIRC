f35594859084da1b007ac0661f039045eddc3906
TheStami <a15@int.pl>
2026-08-19 09:56:39 +0200

    Replace profile picture with cog picture for settings

--- src/components/navigation/navigation-sidebar.tsx
+++ src/components/navigation/navigation-sidebar.tsx
@@ -1,7 +1,9 @@
+import { Settings } from "lucide-react";
+
 import { ScrollArea } from "@/components/ui/scroll-area";
 import { ModeToggle } from "@/components/mode-toggle";
 import { Separator } from "@/components/ui/separator";
-import { UserAvatar } from "@/components/user-avatar";
+import { ActionTooltip } from "@/components/action-tooltip";
 import { useMockStore } from "@/lib/mock-store";
 import { useModal } from "@/hooks/use-modal-store";
 
@@ -10,7 +12,6 @@ import { NavigationItem } from "./navigation-item";
 
 export const NavigationSidebar = () => {
   const servers = useMockStore((state) => state.servers);
-  const currentProfile = useMockStore((state) => state.currentProfile);
   const { onOpen } = useModal();
 
   return (
@@ -34,17 +35,16 @@ export const NavigationSidebar = () => {
       </ScrollArea>
       <div className="pb-3 mt-auto flex items-center flex-col gap-y-4">
         <ModeToggle />
-        <div
-          onClick={() => onOpen("settings")}
-          className="relative group cursor-pointer"
-          title={`${currentProfile.name} (Settings)`}
-        >
-          <UserAvatar
-            src={currentProfile.imageUrl}
-            name={currentProfile.name}
-            className="h-[48px] w-[48px] border-2 border-indigo-500/50 hover:border-indigo-500 transition"
-          />
-        </div>
+        <ActionTooltip side="right" align="center" label="Settings">
+          <button
+            onClick={() => onOpen("settings")}
+            className="group flex items-center justify-center"
+          >
+            <div className="flex mx-3 h-[48px] w-[48px] rounded-[24px] group-hover:rounded-[16px] transition-all overflow-hidden items-center justify-center bg-background dark:bg-neutral-700 group-hover:bg-indigo-500">
+              <Settings className="text-zinc-500 dark:text-zinc-400 group-hover:text-white transition" size={24} />
+            </div>
+          </button>
+        </ActionTooltip>
       </div>
     </div>
   );
