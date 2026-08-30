932121f31b85a19f98a21f8eae5f889af88ac3af
TheStami <a15@int.pl>
2026-08-30 23:09:36 +0200

    Change logo

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -3920,6 +3920,9 @@ pub fn run() {
         ])
         .setup(|app| {
             if let Some(window) = app.get_webview_window("main") {
+                if let Some(icon) = app.default_window_icon() {
+                    let _ = window.set_icon(icon.clone());
+                }
                 let _ = window.show();
                 let _ = window.set_focus();
             }
